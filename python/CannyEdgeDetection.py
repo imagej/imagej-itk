7d788388f5147b4f312c18c2d8f4cec4e65b21a2
Brian Northan <bnorthan@gmail.com>
2015-09-09 10:08:10 -0400

    Add vector params

--- python/CannyEdgeDetection.py
+++ python/CannyEdgeDetection.py
@@ -1,13 +1,21 @@
 # @org.itk.simple.Image image
 # @OUTPUT Dataset output
 
-from org.itk.simple import SmoothingRecursiveGaussianImageFilter
+from org.itk.simple import CannyEdgeDetectionImageFilter
+from org.itk.simple import VectorDouble
+
 import org.itk.simple as sitk
 
-#itkGauss = SmoothingRecursiveGaussianImageFilter();
-edges = sitk.CannyEdgeDetection(image, lowerThreshold=0.0, 
-                                upperThreshold=200.0, variance = (5.0,5.0,5.0))
+canny=CannyEdgeDetectionImageFilter()
 
+vec1=VectorDouble()
+vec2=VectorDouble()
+
+vec1.push_back(0.0)
+vec1.push_back(200.0)
+vec2.push_back(0.2)
+vec2.push_back(0.5)
 
 # call itk rl using simple itk wrapper
-#output = itkGauss.execute(image, 3.0, False);
+output = canny.execute(image);
+#output = canny.execute(image, 0.0, 200.0, vec1, vec2);
